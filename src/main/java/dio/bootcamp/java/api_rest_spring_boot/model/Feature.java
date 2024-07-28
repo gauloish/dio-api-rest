fbaacaccfb15826be280f3ed76a7f5085fd3f454
gauloish <125079293+gauloish@users.noreply.github.com>
2024-07-27 23:49:27 -0300

    criação do model Feature

---
+++ src/main/java/dio/bootcamp/java/api_rest_spring_boot/model/Feature.java
@@ -0,0 +1,12 @@
+package dio.bootcamp.java.api_rest_spring_boot.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Feature {
+    private String name;
+    private String description;
+    private String icon;
+}
