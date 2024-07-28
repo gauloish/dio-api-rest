77d6938dc13b69dff1cc9d75830c1a4891711894
gauloish <125079293+gauloish@users.noreply.github.com>
2024-07-27 23:09:02 -0300

    criação do README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# API Rest na Nuvem com Spring Boot e Railway
+
+Criação de um API Rest na nuvem com Spring Boot e Railway
---
+++ src/test/java/dio/bootcamp/java/api_rest_spring_boot/ApiRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.bootcamp.java.api_rest_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
