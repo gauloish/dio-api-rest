b1189c56940586e2f4052f29c0f3f939ed3a0a4d
gauloish <125079293+gauloish@users.noreply.github.com>
2024-07-28 01:25:59 -0300

    adição de repository para user

---
+++ src/main/java/dio/bootcamp/java/api_rest_spring_boot/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dio.bootcamp.java.api_rest_spring_boot.repository;
+
+import dio.bootcamp.java.api_rest_spring_boot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
