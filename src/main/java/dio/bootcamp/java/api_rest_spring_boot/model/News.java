e82eb996d83275e512e946f5e647d64e32c0adef
gauloish <125079293+gauloish@users.noreply.github.com>
2024-07-27 23:49:40 -0300

    criação do model News

---
+++ src/main/java/dio/bootcamp/java/api_rest_spring_boot/model/News.java
@@ -0,0 +1,12 @@
+package dio.bootcamp.java.api_rest_spring_boot.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class News {
+    private String title;
+    private String description;
+    private String icon;
+}
